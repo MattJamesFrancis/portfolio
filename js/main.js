7607aacf04caaa363b39bf16d6dd5efea85e088d
Samuel Harrison Wolfe <samuelhwolfe@gmail.com>
2016-03-15 17:48:29 -0700

    added another parent-container selector for projects page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	
-	$('.featured-projects').magnificPopup({
+	$('.featured-projects, .thumbnail-row').magnificPopup({
 		delegate: 'a', // child items selector, by clicking on it popup will open
 		type: 'iframe'
 		// other options
